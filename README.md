3327d8599fe70289f174d9c6aac9eac286540548
JohannesMerz <johannes_merz@gmx.de>
2020-11-13 12:14:01 +0100

    add example

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # react-setstate-rerenders
+
+Install dependencies
+
+```
+yarn
+```
+
+Run
+
+```
+yarn start
+```
+
+Go to 
+
+```
+http://localhost:8080/
+```
+
+You can see the render count in the app itself, for more details open dev tools and have a look at the logs.
+
+Uncomment the different parts in `src/App.js` to inspect different render behaviors.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
+
+function useMyHook() {
+  const [s1, setS1] = useState();
+  const [s2, setS2] = useState();
+  const setFn = useCallback(
+    () => {
+      console.log('set s1');
+      setS1(Date.now());
+      console.log('set s2');
+      setS2(Date.now());
+    },
+    [setS1, setS2]
+  );
+  return setFn;
+}
+
 
 export default function() {
+
+  const renderCount = useRef(0)
+  renderCount.current++
+  console.log('render')
+  const setTwoStates = useMyHook()
+
+  // uncomment either one or the other to see the diff re render behaviour
+
+  // // triggers only one re render
+  // useEffect(() => setTwoStates(), [])
+  // // console.logs are render, set s1, set s2, render
+
+  // // triggers two re render
+  // useEffect(() => setTimeout(setTwoStates), 1)
+  // // console.logs are render, set s1, render, set s2, render 
+
+
   return (
-    <div>hello</div>
+    <div>render count: {renderCount.current}</div>
   );
 }
